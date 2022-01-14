497d4159b9898b5f4025f1c9b3ca352a3a100340
Hector Mario Medina Cabanelas <hector.medina.cabanelas@gmail.com>
2022-01-14 22:24:15 +0100

    OrderItem entity configure

---
+++ src/Entity/OrderItem.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\OrderItemRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * This class implements ApiResource, which means that this class will be published
+ * in the docs, so you can play around and get used to the sintax and parameters
+ * needed to send requests and receive responses.
+ *
+ * @ApiResource(
+ *    collectionOperations={"get", "post"},
+ *    itemOperations={"get"}
+ * )
+ * @ORM\Entity(repositoryClass=OrderItemRepository::class)
+ */
+class OrderItem
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $product;
+
+    /**
+     * @ORM\Column(type="decimal", precision=10, scale=2)
+     */
+    private $quantity;
+
+    /**
+     * @ORM\Column(type="decimal", precision=10, scale=2)
+     */
+    private $price;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getProduct(): ?string
+    {
+        return $this->product;
+    }
+
+    public function setProduct(string $product): self
+    {
+        $this->product = $product;
+
+        return $this;
+    }
+
+    public function getQuantity(): ?string
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(string $quantity): self
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    public function getPrice(): ?string
+    {
+        return $this->price;
+    }
+
+    public function setPrice(string $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+}
